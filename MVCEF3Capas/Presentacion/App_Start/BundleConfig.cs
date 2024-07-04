0f3d16c504efbca3d8cb392c8cca637e3978e1a6
alefra88 <efra.rlz@gmail.com>
2024-07-04 13:08:59 -0600

    agregando TichAjax.js en carpeta tich en scripts, se creo el archivo bundle en bundleConfig y se agregó a la vista _Layout la renderización den bundle

--- MVCEF3Capas/Presentacion/App_Start/BundleConfig.cs
+++ MVCEF3Capas/Presentacion/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace Presentacion
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/Tichbundles/js").Include(
+                          "~/Scripts/Tich/TichAjax.js"));
         }
     }
 }
